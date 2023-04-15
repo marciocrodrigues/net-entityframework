177140c1850216f15c34c4118664003c023ce8fc
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-04-15 20:35:37 -0300

    Entity Framework e Data Annotation

---
+++ Blog/Blog/Data/BlogDataContext.cs
@@ -0,0 +1,30 @@
+﻿using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Blog.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        private string _connectionString;
+        public BlogDataContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        // public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+        // public DbSet<UserRole> UserRoles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(_connectionString);
+            // Mostra a query executada pelo EF
+            //options.LogTo(Console.WriteLine);
+        }
+    }
+}
--- Blog/Blog/Program.cs
+++ Blog/Blog/Program.cs
@@ -1,12 +1,86 @@
-﻿using System;
+﻿using Blog.Data;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
 
 namespace Blog
 {
-    internal class Program
+    public class Program
     {
+        private static string CONNECTION_STRING_ID = Environment.GetEnvironmentVariable("CONNECTION_STRING_SQL_SERVER_ID", EnvironmentVariableTarget.Machine);
+        private static string CONNECTION_STRING_PASSWORD = Environment.GetEnvironmentVariable("CONNECTION_STRING_SQL_SERVER_PASSWORD", EnvironmentVariableTarget.Machine);
+        private static string connectionString = @$"Server=localhost, 1433;Database=Blog;User ID={CONNECTION_STRING_ID};Password={CONNECTION_STRING_PASSWORD};TrustServerCertificate=True";
+
+        private static BlogDataContext _context;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _context = new BlogDataContext(connectionString);
+
+            ReadUsers();
+            Console.ReadKey();
+        }
+
+        static void CreateTag()
+        {
+            var tag = new Tag { Name = "ASP.NET", Slug = "aspnet" };
+
+            _context.Tags.Add(tag);
+            _context.SaveChanges();
+        }
+
+        static void UpdateTag(int id, Tag alterTag)
+        {
+            var tag = _context.Tags.FirstOrDefault(x => x.Id == id);
+            tag.Name = alterTag.Name;
+            tag.Slug = alterTag.Slug;
+            _context.Tags.Update(tag);
+            _context.SaveChanges();
+        }
+
+        static void ReadTags()
+        {
+            var tags = _context.Tags.AsNoTracking().ToList();
+
+            foreach (var item in tags)
+            {
+                Console.WriteLine($"{item.Id} - {item.Name} - {item.Slug}");
+            }
+        }
+
+        static void DeleteTag(int id)
+        {
+            var tag = _context.Tags.FirstOrDefault(x => x.Id == id);
+            _context.Remove(tag);
+            _context.SaveChanges();
+        }
+
+        static void ReadUsers()
+        {
+            var users = _context.Users
+                .AsNoTracking()
+                .ToList();
+
+            foreach (var item in users)
+            {
+                Console.WriteLine($"{item.Id} - {item.Name} - {item.Slug}");
+            }
+        }
+
+        static void ReadPosts()
+        {
+            var posts = _context.Posts
+                .AsNoTracking()
+                .Include(x => x.Author)
+                .Include(x => x.Category)
+                .OrderBy(x => x.LastUpdateDate)
+                .ToList();
+
+            foreach (var item in posts)
+            {
+                Console.WriteLine($"{item.Id} - {item.Title} - {item.Author?.Name} - {item.Category?.Name}");
+            }
         }
     }
 }
